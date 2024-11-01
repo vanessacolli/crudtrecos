8ed26fc08ac9813a193c2325214e206f3ffd7bd6
vanessacolli <vcolli.2011@gmail.com>
2024-11-01 11:46:26 -0300

    aula 13

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+# Importa as dependências
+from flask import Flask, render_template
 
+# Criar um aplicativo chamado 'app' do tipo 'Flask'
 app = Flask(__name__)
 
+# Processa a rota "raiz" executando a função logo abaixo
 @app.route("/")
-def hello_world():
-    return "<p> Olá Mundo!</p>"
+def home(): # Função a ser executada
+
+    page = {
+        'titulo': 'JocaTrecos'
+    }
+    # retorno para o navegador
+    return render_template('_template.html',**page)
+
+# Executa o servidor de teste
+if __name__ == '__main__':
+    app.run(debug=True)
