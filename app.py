0520ab0d53cdfa8b5cb69b81b2c5031238d6b453
vanessacolli <vcolli.2011@gmail.com>
2024-11-04 11:44:07 -0300

    aula 14

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # Rota raiz, equivalente a páginai inicial do site (index)
+
+
 @app.route("/")
-def home(): # Função executada ao acessar a rota raiz
+def home():  # Função executada ao acessar a rota raiz
 
     # Cria um usuário "fake" para testes
     # No futuro, isso virá de um cookie
@@ -14,7 +16,7 @@ def home(): # Função executada ao acessar a rota raiz
         'nome': 'Joca da Silva',
         'id': '1'
     }
-    
+
     # Extrai apenas o primeiro nome do usuário
     usuario['pnome'] = usuario['nome'].split()[0]
 
@@ -25,7 +27,30 @@ def home(): # Função executada ao acessar a rota raiz
     }
 
     # Renderiza o template HTML, passaod valores para ele
-    return render_template('_template.html', **pagina)
+    return render_template('home.html', **pagina)
+
+
+@app.route('/novo')
+def novo():
+  # Cria um usuário "fake" para testes
+    # No futuro, isso virá de um cookie
+    usuario = {
+        'nome': 'Joca da Silva',
+        'id': '1'
+    }
+
+    # Extrai apenas o primeiro nome do usuário
+    usuario['pnome'] = usuario['nome'].split()[0]
+
+    # Dados, variáveis e valores a serem passados para o template HTML
+    pagina = {
+        'titulo': 'CRUDTrecos',
+        'usuario': usuario
+    }
+
+    # Renderiza o template HTML, passaod valores para ele
+    return render_template('home.html', **pagina)
+
 
 # Executa o servidor HTTP se estiver no modo de desenvolvimento
 # Remova / comente essas linhas no modo de produção
