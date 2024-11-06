30f9a35672e28d0c040f4d03b5fadf9b396398d0
vanessacolli <vcolli.2011@gmail.com>
2024-11-06 11:37:14 -0300

    aula 16

--- app.py
+++ app.py
@@ -1,8 +1,38 @@
 # Importa as dependências do aplicativo
 from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
 
 # Cria um aplicativo Flask chamado "app"
 app = Flask(__name__)
+
+# Configurações de acesso ao MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'crudtrecos'
+
+# Setup da conexão com MySQL
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+app.config['MYSQL_USE_UNICODE'] = True
+app.config['MYSQL_CHARSET'] = 'utf8mb4'
+
+# Variável de conexão com o MySQL
+mysql = MySQL(app)
+
+
+@app.before_request
+def start():
+
+    # Setup do MySQL para corrigir acentuação
+    cur = mysql.connection.cursor()
+    cur.execute("SET NAMES utf8mb4")
+    cur.execute("SET character_set_connection=utf8mb4")
+    cur.execute("SET character_set_client=utf8mb4")
+    cur.execute("SET character_set_results=utf8mb4")
+
+    # Setup do MySQL para dias da semana e meses em português
+    cur.execute("SET lc_time_names = 'pt_BR'")
+
 
 # Cria um usuário "fake" para testes
 # No futuro, isso virá de um cookie
@@ -16,6 +46,20 @@ usuario['pnome'] = usuario['nome'].split()[0]
 
 @app.route("/")  # Rota raiz, equivalente a página inicial do site (index)
 def index():  # Função executada ao acessar a rota raiz
+
+    # Um SQL de teste para exibir todos os 'trecos' do usuário conectado
+    sql = '''
+        SELECT * FROM treco
+        WHERE t_usuario = '1' 
+            AND t_status = 'on';
+    '''
+    cur = mysql.connection.cursor()
+    cur.execute(sql,)
+    trecos = cur.fetchall()
+    cur.close()
+
+    # Teste de mesa para verificar o retorno dos dados do banco de dados
+    print('\n\n\n', trecos, '\n\n\n')
 
     # Dados, variáveis e valores a serem passados para o template HTML
     pagina = {
@@ -85,9 +129,10 @@ def novasenha():
 
     return render_template('novasenha.html', **pagina)
 
+
 @app.route('/perfil')
 def perfil():
-    
+
     # Dados, variáveis e valores a serem passados para o template HTML
     pagina = {
         'titulo': 'CRUDTrecos - Novo Treco',
